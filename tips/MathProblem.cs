03e56151532583a4fbeb516eace017eaa06d4fd1
Jovan Petkoski <petkoski15@hotmail.com>
2019-08-19 09:54:38 +0200

    The math problem CH07-03

--- tips/MathProblem.cs
+++ tips/MathProblem.cs
@@ -15,6 +15,16 @@ namespace Tips
             Console.WriteLine(result);
         }
 
+        /**
+         * When writing math code (that uses math operations, like SampledAverage())
+         * you have to overload methods with different types of parameters (double in
+         * our case, write separate method for ints / floats / etc., if required). 
+         * Don't use generic type parameter, because C# compiler doesn't know if
+         * the generic type T has those operators (+=, divison, etc.)
+         * When it comes to operators like division, multip., subtr. accross the built
+         * in types (like int / double / float), the best solution is NOT generic type
+         * paremeters, but overloaded methods.
+         */
         private static double SampledAverage(double[] numbers)
         {
             var count = 0;

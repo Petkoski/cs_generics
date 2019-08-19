2e1d9507f49bf9ba788e7f36e23c2c74d1bbb0e8
Jovan Petkoski <petkoski15@hotmail.com>
2019-08-19 10:16:23 +0200

    Using base types CH07-04

--- tips/Program.cs
+++ tips/Program.cs
@@ -6,22 +6,24 @@ using System.Threading.Tasks;
 
 namespace Tips
 {
-   
-
     class Program
     {
         static void Main(string[] args)
         {
+            var list = new List<Item>();
+
+            //Adding different type items to a list
             var a = new Item<int>();
             var b = new Item<int>();
             var c = new Item<string>();
 
-            Console.WriteLine(Item.InstanceCount);
-            Console.WriteLine(Item.InstanceCount);
+            list.Add(a);
+            list.Add(b);
+            list.Add(c);
 
+            Console.WriteLine(Item.InstanceCount);
+            Console.WriteLine(Item.InstanceCount);
         }
-
-        
     }
 
     public class Item<T> : Item
